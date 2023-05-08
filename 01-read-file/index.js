123778bb52af5a11f26e5141e3cc267c18f771d4
lizavr <elizavetarabetskaya@gmail.com>
2023-05-08 13:56:14 +0400

    feat: add task 4

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,10 +1,8 @@
 const path = require('path');
 const fs = require('fs');
-
 const way = path.join(__dirname, 'text.txt');
-
-const readSteam = fs.createReadStream(way, 'utf-8');
+const readStream = fs.createReadStream(way, 'utf-8');
 let data = '';
-readSteam.on('data', (chunk) => (data += chunk));
-readSteam.on('end', () => console.log(data));
-readSteam.on('error', (error) => console.log('Error', error.message));
+readStream.on('data', (chunk) => (data += chunk));
+readStream.on('end', () => console.log(data));
+readStream.on('error', (error) => console.log('Error', error.message));
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,37 @@
+const path = require('path');
+const fs = require('fs');
+const copyFrom = path.join(__dirname, 'files');
+const copyTo = path.join(__dirname, 'files-copy');
 
+const copyDirectory = (copyFrom, copyTo) => {
+  fs.mkdir(copyTo,{ recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  fs.readdir(copyFrom, (err, files) => {
+    files.forEach((file) => {
+      if (err) {
+        console.error(err);
+      }
+      fs.stat(path.join(copyFrom, file), (err, stats) => {
+        if (err) {
+          console.log(err);
+        }
+        if (stats.isDirectory()) {
+         copyDirectory(path.join(copyFrom, file), path.join(copyTo, file));
+         return;
+        }
+        fs.copyFile(
+          path.join(copyFrom, file),
+          path.join(copyTo, file),
+          (err, stats) => {
+            if (err) {
+              console.log(err);
+            }
+          }
+        );
+      });
+    });
+  });
+};
+copyDirectory(copyFrom, copyTo);
